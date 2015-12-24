21ac48e4e4bfb570f3eb6897c35a6b16da76dfbe
HungNV.FPT <HungNV.FPT@HUNGNV-FPT.NOR.TPB.COM>
2015-12-24 17:17:00 +0700

    update lib

---
+++ src/com/dgs/admin/controllers/ChangePagesController.java
@@ -0,0 +1,17 @@
+package com.dgs.admin.controllers;
+
+import javax.ws.rs.Path;
+
+import org.apache.log4j.Logger;
+
+@Path("/changePages")
+public class ChangePagesController {
+
+	/**
+	 * author : hungnv 
+	 * created on : 06/10/2015
+	 */
+	final static Logger LOGGER = Logger.getLogger(ChangePagesController.class);
+	
+	
+}
